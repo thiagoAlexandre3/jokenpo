3beff0aa0ca909c32cd3dd64fefb4866c784091a
thiagoAlexandre3 <thiagodev3@gmail.com>
2026-03-09 06:05:00 -0300

    completando lógica do botão Jogar

--- scripts/script_partida.js
+++ scripts/script_partida.js
@@ -1,5 +1,6 @@
 let jogada = 0;
 let vencedor = "";
+let jogou = true;
 
 //recebe jogada
 function fpedra(){
@@ -14,23 +15,32 @@ function ftesoura(){
 
 //envia jogada e recebe resultado
 function resultado(){
-    if (jogada == 0){
-        alert("Selecione sua jogada antes de comparar");
-    }else{
-        //desabilita botao
-        document.getElementById('btn_compara').disabled = true;
+    if(jogou){
+        if (jogada == 0){
+                alert("Selecione sua jogada antes de comparar");
+            }else{
+                //desabilita botao
+                document.getElementById('btn_compara').innerHTML = "Aguardando jogada...";
+                document.getElementById('btn_compara').disabled = true;
 
-        const dados = JSON.stringify(jogada);
-        $.ajax({
-            url: './api/vencedor.php',
-            type: 'POST',
-            data: {data: dados},
-            success: function(result){
-                document.getElementById('btn_compara').innerHTML = "jogador: " + result.vencedor + ", venceu";
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
+                const dados = JSON.stringify(jogada);
+                $.ajax({
+                    url: './api/vencedor.php',
+                    type: 'POST',
+                    data: {data: dados},
+                    success: function(result){
+                        jogou = false;
+                        document.getElementById('btn_compara').innerHTML = "Jogar novamente";
+                        document.getElementById('btn_compara').disabled = false;
+                        //document.getElementById('btn_compara').innerHTML = "jogador: " + result.vencedor + ", venceu";
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
 
+                    }
+                });
             }
-        });
+    }else{
+        window.location.href = "./index.php";
     }
+    
 }
